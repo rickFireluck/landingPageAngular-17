cb535ae0b33719889f8ce8f9e621bd4f83816783
rickFireluck <159982081+rickFireluck@users.noreply.github.com>
2024-09-09 14:41:54 -0500

    Landing Page Angular 17 v1

---
+++ src/app/pages/contact/contact.component.ts
@@ -0,0 +1,37 @@
+import { CommonModule, NgClass } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-contact',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, NgClass],
+  templateUrl: './contact.component.html',
+  styleUrl: './contact.component.css'
+})
+export class ContactComponent implements OnInit{
+
+  contactForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.contactForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', [Validators.required, Validators.minLength(10)]]
+    })
+  }
+
+  ngOnInit(): void {
+
+  }
+
+    //En este caso, el field debe coincidir con el formControlName del input en el HTML
+  hasErrors(field: string, typeError: string){
+    return this.contactForm.get(field)?.hasError(typeError) && this.contactForm.get(field)?.touched;
+  }
+
+  enviar(event: Event){
+    event.preventDefault(); //evita que la pagina se actualice al aplastar el boton de submit
+    console.log(this.contactForm.value);
+  }
+
+}
